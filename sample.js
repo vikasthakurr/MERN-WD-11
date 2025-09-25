561d0cf673095a77297f34f9c000974b0e6b272c
Dotjson01 <sahilali88827@gmail.com>
2025-09-25 17:36:16 +0530

    sample.js

---
+++ sample.js
@@ -0,0 +1,9 @@
+// This is a sample JavaScript file
+
+// Function to greet the user
+function greetUser(name) {
+    console.log(`Hello, ${name}! Welcome to the MERN-WD-11 project.`);
+}
+
+// Call the function with a sample name
+greetUser("User");
